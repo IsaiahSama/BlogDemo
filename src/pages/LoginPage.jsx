19ec3fd42fefcfa394c2a4d252494a13bf8dd64c
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-06 17:36:07 -0400

    Updated Login Page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,9 +2,29 @@ import React from "react";
 
 const LoginPage = () => {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <form>
+            <h1>Register with us!</h1>
+
+            <div class="field">
+                <label class="label">Email <span class="has-text-danger">*</span></label>
+                <div class="control">
+                    <input class="input" type="email" name="email" id="email" placeholder="Email" required />
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Password <span class="has-text-danger">*</span></label>
+                <div class="control">
+                    <input class="input" type="password" name="password" id="password" placeholder="Password" required />
+                </div>
+            </div>
+
+            <div class="field is-grouped">
+                <div class="control">
+                    <button class="button is-link">Login</button>
+                </div>
+            </div>
+        </form>
     );
 }
 
