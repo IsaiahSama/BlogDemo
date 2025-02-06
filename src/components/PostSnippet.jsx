de7527f2a52f1811f8e7e2bca139746d38f8d7a7
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-06 17:53:00 -0400

    Can now navigate to Post from PostSnippet

--- src/components/PostSnippet.jsx
+++ src/components/PostSnippet.jsx
@@ -1,24 +1,29 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const PostSnippet = () => {
+
+    const navigate = useNavigate();
+
     return (
-        <div class="card">
-            <div class="card-content">
-                <div class="media">
-                    <div class="media-content">
-                        <p class="title is-4">Why CSS Is Awesome</p>
-                        <p class="subtitle is-6">Written by Jeffery</p>
-                        <p>
-                            Date: <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-                        </p>
+        <div class="card" onClick={() => navigate("/post")}>
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-content">
+                            <p class="title is-4">Why CSS Is Awesome</p>
+                            <p class="subtitle is-6">Written by Jeffery</p>
+                            <p>
+                                Date: <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="content snippet">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe aliquam in inventore impedit, fugit non quas architecto iusto. Fugit corrupti temporibus provident itaque ullam quaerat, obcaecati consectetur odio laborum nostrum!
                     </div>
                 </div>
-
-                <div class="content snippet">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe aliquam in inventore impedit, fugit non quas architecto iusto. Fugit corrupti temporibus provident itaque ullam quaerat, obcaecati consectetur odio laborum nostrum!
-                </div>
-            </div>
         </div>
+
     );
 }
 
