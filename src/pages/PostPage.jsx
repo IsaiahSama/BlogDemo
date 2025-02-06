bbdec6809917b7e97cb3f9b0d7b2c491478f5719
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-06 17:48:38 -0400

    Added Post Page

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -2,9 +2,21 @@ import React from "react";
 
 const PostPage = () => {
     return (
-        <div>
-            <h1>Post</h1>
-        </div>
+        <>
+            <h1 class="title">Why CSS Is Awesome</h1>
+            <p class="subtitle is-6">Written by Jeffery</p>
+            <p>
+                Date: <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </p>
+            <div class="content">
+                Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu. Modus argumentum ne qui tation efficiendi in eos. Ei mea falli legere efficiantur et tollit aliquip debitis mei. No deserunt mediocritatem mel. Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu. Modus argumentum ne qui tation efficiendi in eos. Ei mea falli legere efficiantur et tollit aliquip debitis mei. No deserunt mediocritatem mel. Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu. Modus argumentum ne qui tation efficiendi in eos. Ei mea falli legere efficiantur et tollit aliquip debitis mei. No deserunt mediocritatem mel. Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu. Modus argumentum ne qui tation
+            </div>
+
+            <div className="is-flex is-justify-content-space-around">
+                <button className="button is-success">Like 👍</button>
+                <button className="button is-danger">Dislike 👎</button>
+            </div>
+        </>
     );
 }
 
