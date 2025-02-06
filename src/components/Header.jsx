feb090fb27472270d2b1623112704c9322d3df86
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-06 16:37:39 -0400

    Added styling for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,54 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+        <>
+
+            <nav className="navbar" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a className="navbar-item" href="/">
+                        <img src="/cssLogo.png" alt="CSS Logo" style={{ width: "50px", height: "50px" }} />
+                        <p>CSS Blog</p>
+                    </a>
+
+                    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-start">
+                        <a className="navbar-item">
+                            Home
+                        </a>
+
+                        <a className="navbar-item">
+                            New Post
+                        </a>
+
+                        <a className="navbar-item">
+                            Profile
+                        </a>
+                    </div>
+
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <div className="buttons">
+                                <a className="button is-primary">
+                                    <strong>Sign up</strong>
+                                </a>
+                                <a className="button is-light">
+                                    Log in
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+        </>
     );
 }
 
