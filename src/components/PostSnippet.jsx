1f8f9e05af05ff7ab79fe72404005f7ebe1bc20b
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-06 17:14:49 -0400

    Styled home page and added post snippets

---
+++ src/components/PostSnippet.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const PostSnippet = () => {
+    return (
+        <div class="card">
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4">Why CSS Is Awesome</p>
+                        <p class="subtitle is-6">Written by Jeffery</p>
+                        <p>
+                            Date: <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="content">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe aliquam in inventore impedit, fugit non quas architecto iusto. Fugit corrupti temporibus provident itaque ullam quaerat, obcaecati consectetur odio laborum nostrum!
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PostSnippet
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import PostSnippet from "../components/PostSnippet";
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+        <>
+            <div className="is-flex is-justify-content-space-between">
+                <h1>Latest Posts</h1>
+
+                <div className="is-flex is-flex-direction-column">
+                    <p>Welcome Jeffery</p>
+                    <input type="text" name="search" id="search" placeholder="Search" className="input" />
+                </div>
+            </div>
+            <div className="posts py-2">
+                <PostSnippet />
+                <PostSnippet />
+                <PostSnippet />
+
+            </div>
+        </>
     );
 }
 
