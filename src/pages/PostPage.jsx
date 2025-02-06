3591f7eb1e8a0a480197e502968ab57076841103
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-06 18:11:02 -0400

    Post page now goes dynamically

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,15 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const PostPage = ( {posts} ) => {
+
+    const { id } = useParams();
+    const post = posts.find(post => post.id === parseInt(id));
+
     return (
         <>
-            <h1 class="title">Why CSS Is Awesome</h1>
-            <p class="subtitle is-6">Written by Jeffery</p>
+            <h1 class="title">{post.title}</h1>
+            <p class="subtitle is-6">Written by {post.author}</p>
             <p>
-                Date: <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                Date: <time datetime={post.date}>{post.date}</time>
             </p>
             <div class="content">
-                Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu. Modus argumentum ne qui tation efficiendi in eos. Ei mea falli legere efficiantur et tollit aliquip debitis mei. No deserunt mediocritatem mel. Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu. Modus argumentum ne qui tation efficiendi in eos. Ei mea falli legere efficiantur et tollit aliquip debitis mei. No deserunt mediocritatem mel. Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu. Modus argumentum ne qui tation efficiendi in eos. Ei mea falli legere efficiantur et tollit aliquip debitis mei. No deserunt mediocritatem mel. Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu. Modus argumentum ne qui tation
+                {post.content}
             </div>
 
             <div className="is-flex is-justify-content-space-around">
