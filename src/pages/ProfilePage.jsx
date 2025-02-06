f37063b56f72bf3f075781686266c74ecdf65603
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-06 17:28:42 -0400

    Added Profile page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,9 +2,17 @@ import React from "react";
 
 const ProfilePage = () => {
     return (
-        <div>
+        <>
             <h1>Profile</h1>
-        </div>
+            <p>Welcome Jeffery CSS</p>
+            <p>Number of Posts: 5</p>
+
+            <h2>About</h2>
+            <p>
+            Lorem ipsum dolor sit amet et delectus accommodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute copiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum moderatius cu. Sit munere facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui posse omnium mediocritatem est cu. Modus argumentum ne qui tation efficiendi in eos.
+            </p>
+            <button className="button is-warning">Logout</button>
+        </>
     );
 }
 
