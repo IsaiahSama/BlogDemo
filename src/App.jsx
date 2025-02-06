19abdaf6a806fceece43a2c7f99bf991ff44afc7
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-06 16:12:13 -0400

    Added routes and stubbed each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
 import './App.css'
 import React from "react"
-import Home from "./Home"
+import HomePage from "./pages/HomePage"
+import NewPostPage from "./pages/NewPostPage"
+import PostPage from "./pages/PostPage"
+import RegisterPage from "./pages/RegisterPage"
+import LoginPage from "./pages/LoginPage"
+import ProfilePage from "./pages/ProfilePage"
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 import {
 	BrowserRouter as Router,
 	Routes,
 	Route,
-	Link,
-	Outlet,
 } from "react-router-dom"
 
 function App() {
 
 	return (
 		<Router>
+			<Header />
 				<Routes>
-					<Route path="/" element={<Home />} />	
+					<Route path="/" element={<HomePage />} />
+					<Route path="/post/create" element={<NewPostPage />} />
+					<Route path="/post" element={<PostPage />} />
+					<Route path="/register" element={<RegisterPage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/profile" element={<ProfilePage />} />
 				</Routes>
+			<Footer />
 		</Router>
 	);
 }
---
+++ src/pages/NewPostPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NewPostPage = () => {
+    return (
+        <div>
+            <h1>NewPost</h1>
+        </div>
+    );
+}
+
+export default NewPostPage
