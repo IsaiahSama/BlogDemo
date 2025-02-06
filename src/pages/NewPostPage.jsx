de842e30dcd7df238bbc57411a2fc1085f659c5b
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-06 17:26:13 -0400

    Added New Post Page

--- src/pages/NewPostPage.jsx
+++ src/pages/NewPostPage.jsx
@@ -2,9 +2,28 @@ import React from "react";
 
 const NewPostPage = () => {
     return (
-        <div>
-            <h1>NewPost</h1>
-        </div>
+        <form>
+            <h1>Create new post</h1>
+            <div class="field">
+                <label class="label">Post Title</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="Title for Post" />
+                </div>
+            </div>
+
+            <div class="field">
+                <label class="label">Post Content</label>
+                <div class="control">
+                    <textarea class="textarea" placeholder="Type your content here" rows="15" />
+                </div>
+            </div>
+
+            <div class="field is-grouped">
+                <div class="control">
+                    <button class="button is-link">Submit</button>
+                </div>
+            </div>
+        </form>
     );
 }
 
