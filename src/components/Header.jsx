040ae0e79db6f22a1207806bfd4c3d5729ec6afc
IsaiahSama <zeldaplayergl@gmail.com>
2025-02-06 16:53:17 -0400

    Changed styling and removed max width

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,54 +2,50 @@ import React from "react";
 
 const Header = () => {
     return (
-        <>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a className="navbar-item" href="/">
+                    <img src="/cssLogo.png" alt="CSS Logo" style={{ width: "50px", height: "50px" }} />
+                    <p>CSS Blog</p>
+                </a>
 
-            <nav className="navbar" role="navigation" aria-label="main navigation">
-                <div className="navbar-brand">
+                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
                     <a className="navbar-item" href="/">
-                        <img src="/cssLogo.png" alt="CSS Logo" style={{ width: "50px", height: "50px" }} />
-                        <p>CSS Blog</p>
+                        Home
                     </a>
 
-                    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
+                    <a className="navbar-item" href="/post/create">
+                        New Post
+                    </a>
+
+                    <a className="navbar-item" href="/profile">
+                        Profile
                     </a>
                 </div>
 
-                <div id="navbarBasicExample" className="navbar-menu">
-                    <div className="navbar-start">
-                        <a className="navbar-item" href="/">
-                            Home
-                        </a>
-
-                        <a className="navbar-item" href="/post/create">
-                            New Post
-                        </a>
-
-                        <a className="navbar-item" href="/profile">
-                            Profile
-                        </a>
-                    </div>
-
-                    <div className="navbar-end">
-                        <div className="navbar-item">
-                            <div className="buttons">
-                                <a className="button is-primary">
-                                    <strong>Sign up</strong>
-                                </a>
-                                <a className="button is-light">
-                                    Log in
-                                </a>
-                            </div>
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <a className="button is-primary">
+                                <strong>Sign up</strong>
+                            </a>
+                            <a className="button is-light">
+                                Log in
+                            </a>
                         </div>
                     </div>
                 </div>
-            </nav>
-
-        </>
+            </div>
+        </nav>
     );
 }
 
